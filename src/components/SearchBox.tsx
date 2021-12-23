217318ca0acb1f88cf4909d7c51a062dd5887647
Ali Poormohammadi <poormohammmadi@gmail.com>
2021-12-23 16:06:52 +0400

    add searchbox component

---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+
+type Props = {
+    value?: string;
+    onChange: (e: string) => void;
+    onSubmit: () => void;
+}
+
+export default function SearchBox(props: Props) {
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                value={props.value}
+                onChangeText={props.onChange}
+            />
+            <Button
+                title="Search"
+                onPress={props.onSubmit}
+            />
+        </View >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: 'grey',
+        paddingLeft: 10,
+    }
+});
