dad4afbf798e1ab02bc133ce0d49013ae8b15c8d
Ali Poormohammadi <poormohammmadi@gmail.com>
2021-12-23 18:43:26 +0400

    add commnets to weather api

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -5,6 +5,10 @@ import { DailyForecastResponse } from '../utils/models/daily-forecast-response';
 
 const apiKey = '828e2c4915547cba5b8e0d8ccc3ffac5';
 
+// The best way to implement this is using Hourly Forecast, 4-day API format (https://openweathermap.org/api/hourly-forecast)
+// But it's not free so I had to implement multiple functions to get the 
+// geographical coordinates of the city and then get its 7-day weather forecast
+
 const getForecast = async (city: string): Promise<DailyForecastResponse> => {
     return new Promise((resolve, reject) => {
         searchCity(city)
